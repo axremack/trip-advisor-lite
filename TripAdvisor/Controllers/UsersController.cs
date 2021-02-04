640a95d18092309eaf4675b1e3070cfabbede445
Axel Remack <Axel.REMACK@etu.uca.fr>
2021-02-04 18:24:06 +0100

    Code préliminaire controller user

--- TripAdvisor/Controllers/UsersController.cs
+++ TripAdvisor/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +15,79 @@ namespace TripAdvisor.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly TripAdvisorContext _repository;
+
+        public UsersController(TripAdvisorContext context)
+        {
+            _repository = context;
+        }
         // GET: /<UsersController>
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        public IEnumerable<User> Get() =>
+            _repository.Users.ToList();
 
         // GET /<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetById(int id)
         {
-            return "value";
+            var item = _repository.Users.SingleOrDefault(t => t.UserId == id);
+            if (item == null)
+            {
+                return NotFound("User not found");
+            }
+            return new ObjectResult(item);
         }
 
         // POST /<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<User>> Post([FromBody] User user)
         {
+            UserValidator validator = new UserValidator();
+            ValidationResult result = validator.Validate(user);
+            if (!result.IsValid)
+            {
+                foreach (var failure in result.Errors)
+                {
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                }
+            }
+
+            _repository.Users.Add(user);
+            await _repository.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(Get), new { id = user.UserId }, user);
         }
 
         // PUT /<UsersController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<User>> Put(int id, [FromBody] User user)
         {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _repository.Entry(user).State = EntityState.Modified;
+            await _repository.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = user.UserId }, user);
         }
 
         // DELETE /<UsersController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<User>> Delete(int id)
         {
+            var p = await _repository.Users.FindAsync(id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Users.Remove(p);
+            await _repository.SaveChangesAsync();
+
+            return p;
         }
     }
 }
