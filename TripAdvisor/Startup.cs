fcbe220e69e1d65ba8cd5ed754e7761553ba0c46
Axel Remack <Axel.REMACK@etu.uca.fr>
2021-02-05 13:58:29 +0100

    Début connexion DAL

--- TripAdvisor/Startup.cs
+++ TripAdvisor/Startup.cs
@@ -22,8 +22,8 @@ namespace TripAdvisor
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			/*services.AddDbContext<TripAdvisorContext>(options =>
-				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));*/
+			services.AddDbContext<TripAdvisorContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
