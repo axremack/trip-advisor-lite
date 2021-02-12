6ae17fda094c4cc9c5da1636deeb40e4435a6bc1
Cédric Klodzinski <cedric99@sfr.fr>
2021-02-12 16:12:51 +0100

    Front Login

--- TripAdvisor.Front/ClientApp/src/App.js
+++ TripAdvisor.Front/ClientApp/src/App.js
@@ -4,19 +4,19 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
-import './custom.css'
+import { Login } from './components/Login';
 
 export default class App extends Component {
-  static displayName = App.name;
+	static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+	render() {
+		return (
+			<Layout>
+				<Route exact path='/' component={Home} />
+				<Route path='/counter' component={Counter} />
+				<Route path='/fetch-data' component={FetchData} />
+				<Route path='/login' component={Login} />
+			</Layout>
+		);
+	}
 }
