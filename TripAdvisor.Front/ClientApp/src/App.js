53e8ffc25a9c019aa3d391038dfeb1866d5879b0
Stacy Varloteaux <stacychnza@outlook.fr>
2021-02-18 12:03:33 +0100

    Creation formplace  Création ViewForm et modif DAL?

--- TripAdvisor.Front/ClientApp/src/App.js
+++ TripAdvisor.Front/ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { Login } from './components/Login';
+import { PlaceForm } from './components/PlaceForm';
 
 export default class App extends Component {
 	static displayName = App.name;
@@ -16,6 +17,7 @@ export default class App extends Component {
 				<Route path='/counter' component={Counter} />
 				<Route path='/fetch-data' component={FetchData} />
 				<Route path='/login' component={Login} />
+				<Route path='/placeform' component={PlaceForm} />
 			</Layout>
 		);
 	}
---
+++ TripAdvisor.Front/ClientApp/src/components/PlaceForm.js
@@ -0,0 +1,57 @@
+﻿import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+export class NavMenu extends Component {
+	static displayName = NavMenu.name;
+
+	constructor(props) {
+		super(props);
+
+		this.toggleNavbar = this.toggleNavbar.bind(this);
+		this.state = {
+			collapsed: true
+		};
+	}
+
+	toggleNavbar() {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
+
+	render() {
+		return (
+			<Form>
+				<Col md={6}>
+					<FormGroup>
+						<Input type="text" name="name" id="exampleName" placeholder="Nom" />
+					</FormGroup>
+				</Col>
+				<Col md={6}>
+					<FormGroup>
+						<Input type="textarea" name="description" id="exampleDescritpion" placeholder="Description" />
+					</FormGroup>
+				</Col>
+				<Col md={6}>
+					<FormGroup>
+						<Input type="text" name="city" id="exampleCity" placeholder="Ville"/>
+					</FormGroup>
+				</Col>
+				<Col md={6}>
+					<FormGroup>
+						<Input type="text" name="placetype" id="examplePlaceType" placeholder="Type" />
+					</FormGroup>
+				</Col>
+				<Col md={6}>
+					<FormGroup>
+						<Input type="text" name="tag" id="exampleTag" placeholder="Tag" />
+					</FormGroup>
+				</Col>
+				<div>
+					<Button color="success">Valider</Button>{' '}
+				</div>	
+			</Form>
+		);
+	}
+}
